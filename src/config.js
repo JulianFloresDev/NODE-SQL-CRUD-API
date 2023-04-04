cfaec98cacc1090fe76b8468e947a30e2b14019f
Julian Flores <julianfloresdev@gmail.com>
2023-04-04 14:41:04 -0300

    feat: refactor configs constants

--- src/config.js
+++ src/config.js
@@ -1,3 +1,12 @@
+import { config } from 'dotenv';
+
+config();
+
 export default {
   port: process.env.PORT || 8001,
+  DDBB_IP: process.env.DDBB_IP,
+  DDBB_USER: process.env.DDBB_USER,
+  DDBB_PASSWORD: process.env.DDBB_PASSWORD,
+  DDBB_PORT: process.env.DDBB_PORT || 3306,
+  DDBB_DATABASE_NAME: process.env.DDBB_DATABASE_NAME,
 };
--- src/db.js
+++ src/db.js
@@ -1,11 +1,10 @@
 import { createPool } from 'mysql2/promise';
-import { config } from 'dotenv';
-config();
+import constants from './config.js';
 
 export const connection = createPool({
-  host: process.env.DDBB_IP,
-  user: process.env.DDBB_USER,
-  password: process.env.DDBB_PASSWORD,
-  port: process.env.DDBB_PORT || 5001,
-  database: process.env.DDBB_DATABASE_NAME,
+  host: constants.DDBB_IP,
+  user: constants.DDBB_USER,
+  password: constants.DDBB_PASSWORD,
+  port: constants.DDBB_PORT,
+  database: constants.DDBB_DATABASE_NAME,
 });
